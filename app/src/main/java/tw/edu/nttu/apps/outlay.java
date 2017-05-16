fd88e600ce47814b3b07b43de28161016521c681
Owlscarf <lf22450@hotmail.com>
2017-05-16 16:29:56 +0800

    支出的計算機功能弄好了

--- app/src/main/java/tw/edu/nttu/apps/outlay.java
+++ app/src/main/java/tw/edu/nttu/apps/outlay.java
@@ -7,28 +7,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
 public class outlay extends AppCompatActivity {
 
-    private Button num_3_bt;
-    private Button num_4_bt;
-    private Button num_5_bt;
-    private Button num_6_bt;
-    private Button num_7_bt;
-    private Button num_8_bt;
-    private Button num_9_bt;
-    private Button num_0_bt;
     private Button plus_bt;     //按鈕加
     private Button minus_bt;    //按鈕減
     private Button times_bt;    //按鈕乘
     private Button divied_bt;   //按鈕除
-    private Button dot_bt;      //小數點
     private Button equals_bt;   //等於
     private Button write_bt;    //計入
     private Button clear_bt;    //清除內容
+    private Button back_bt;     //倒退
     private TextView inputmoney_tx; //金額
     private boolean firstinput = true;  //判定是否第一次輸入
     private boolean contiOperate = false; //判定是否為連續加減乘除
     private int lastmoney; //儲存最後一次計算的金額
+    private int result = 0; //等於鍵時用到的
     private char lastOperate;
 
 
@@ -42,19 +37,19 @@ public class outlay extends AppCompatActivity {
     private void setupView(){
         Button num_1_bt = (Button) findViewById(R.id.num_1_bt);
         Button num_2_bt = (Button) findViewById(R.id.num_2_bt);
-        num_3_bt=(Button) findViewById(R.id.num_3_bt);
-        num_4_bt=(Button) findViewById(R.id.num_4_bt);
-        num_5_bt=(Button) findViewById(R.id.num_5_bt);
-        num_6_bt=(Button) findViewById(R.id.num_6_bt);
-        num_7_bt=(Button) findViewById(R.id.num_7_bt);
-        num_8_bt=(Button) findViewById(R.id.num_8_bt);
-        num_9_bt=(Button) findViewById(R.id.num_9_bt);
-        num_0_bt=(Button) findViewById(R.id.num_0_bt);
+        Button num_3_bt=(Button) findViewById(R.id.num_3_bt);
+        Button num_4_bt=(Button) findViewById(R.id.num_4_bt);
+        Button num_5_bt=(Button) findViewById(R.id.num_5_bt);
+        Button num_6_bt=(Button) findViewById(R.id.num_6_bt);
+        Button num_7_bt=(Button) findViewById(R.id.num_7_bt);
+        Button num_8_bt=(Button) findViewById(R.id.num_8_bt);
+        Button num_9_bt=(Button) findViewById(R.id.num_9_bt);
+        Button num_0_bt=(Button) findViewById(R.id.num_0_bt);
         plus_bt=(Button) findViewById(R.id.plus_bt);
         minus_bt=(Button) findViewById(R.id.minus_bt);
         times_bt=(Button) findViewById(R.id.times_bt);
         divied_bt=(Button) findViewById(R.id.divied_bt);
-        dot_bt=(Button) findViewById(R.id.dot_bt);
+        back_bt=(Button) findViewById(R.id.back_bt);
         equals_bt=(Button) findViewById(R.id.equals_bt);
         write_bt=(Button) findViewById(R.id.write_bt);
         clear_bt=(Button) findViewById(R.id.clear_bt);
@@ -63,10 +58,22 @@ public class outlay extends AppCompatActivity {
 
         num_1_bt.setOnClickListener(num_1_click);
         num_2_bt.setOnClickListener(num_2_click);
+        num_3_bt.setOnClickListener(num_3_click);
+        num_4_bt.setOnClickListener(num_4_click);
+        num_5_bt.setOnClickListener(num_5_click);
+        num_6_bt.setOnClickListener(num_6_click);
+        num_7_bt.setOnClickListener(num_7_click);
+        num_8_bt.setOnClickListener(num_8_click);
+        num_9_bt.setOnClickListener(num_9_click);
+        num_0_bt.setOnClickListener(num_0_click);
 
         plus_bt.setOnClickListener(plus_click);
-
+        minus_bt.setOnClickListener(minus_click);
+        times_bt.setOnClickListener(times_click);
+        divied_bt.setOnClickListener(divied_click);
+        back_bt.setOnClickListener(back_click);
         clear_bt.setOnClickListener(clear_click);
+        equals_bt.setOnClickListener(equals_click);
 
 
     }
@@ -103,6 +110,136 @@ public class outlay extends AppCompatActivity {
             firstinput = false;
         }
     };
+    //3號鍵
+    Button.OnClickListener num_3_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if (firstinput)
+                inputmoney_tx.setText("3");
+            else
+            if (inputmoney_tx.getText() == "0"){
+                inputmoney_tx.setText("3");
+            }
+            else {
+                inputmoney_tx.setText(inputmoney_tx.getText()+"3");
+            }
+            firstinput = false;
+        }
+    };
+    //4號鍵
+    Button.OnClickListener num_4_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if (firstinput)
+                inputmoney_tx.setText("4");
+            else
+            if (inputmoney_tx.getText() == "0"){
+                inputmoney_tx.setText("4");
+            }
+            else {
+                inputmoney_tx.setText(inputmoney_tx.getText()+"4");
+            }
+            firstinput = false;
+        }
+    };
+    //5號鍵
+    Button.OnClickListener num_5_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if (firstinput)
+                inputmoney_tx.setText("5");
+            else
+            if (inputmoney_tx.getText() == "0"){
+                inputmoney_tx.setText("5");
+            }
+            else {
+                inputmoney_tx.setText(inputmoney_tx.getText()+"5");
+            }
+            firstinput = false;
+        }
+    };
+    //6號鍵
+    Button.OnClickListener num_6_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if (firstinput)
+                inputmoney_tx.setText("6");
+            else
+            if (inputmoney_tx.getText() == "0"){
+                inputmoney_tx.setText("6");
+            }
+            else {
+                inputmoney_tx.setText(inputmoney_tx.getText()+"6");
+            }
+            firstinput = false;
+        }
+    };
+    //7號鍵
+    Button.OnClickListener num_7_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if (firstinput)
+                inputmoney_tx.setText("7");
+            else
+            if (inputmoney_tx.getText() == "0"){
+                inputmoney_tx.setText("7");
+            }
+            else {
+                inputmoney_tx.setText(inputmoney_tx.getText()+"7");
+            }
+            firstinput = false;
+        }
+    };
+    //8號鍵
+    Button.OnClickListener num_8_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if (firstinput)
+                inputmoney_tx.setText("8");
+            else
+            if (inputmoney_tx.getText() == "0"){
+                inputmoney_tx.setText("8");
+            }
+            else {
+                inputmoney_tx.setText(inputmoney_tx.getText()+"8");
+            }
+            firstinput = false;
+        }
+    };
+    //9號鍵
+    Button.OnClickListener num_9_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if (firstinput)
+                inputmoney_tx.setText("9");
+            else
+            if (inputmoney_tx.getText() == "0"){
+                inputmoney_tx.setText("9");
+            }
+            else {
+                inputmoney_tx.setText(inputmoney_tx.getText()+"9");
+            }
+            firstinput = false;
+        }
+    };
+    //0號鍵
+    Button.OnClickListener num_0_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if (firstinput)
+                inputmoney_tx.setText("0");
+            else
+            if (inputmoney_tx.getText() == "0"){
+                inputmoney_tx.setText("0");
+            }
+            else {
+                inputmoney_tx.setText(inputmoney_tx.getText()+"0");
+            }
+            firstinput = false;
+        }
+    };
+
+    //+號鍵
     Button.OnClickListener plus_click = new Button.OnClickListener(){
         @Override
         public void onClick(View v) {
@@ -116,12 +253,15 @@ public class outlay extends AppCompatActivity {
                         case '+':
                             temp_money = lastmoney + Integer.parseInt(inputmoney_tx.getText().toString());
                             break;
-                        //case '-':
-
-                        //case '*':
-
-                        //case '/':
-
+                        case '-':
+                            temp_money = lastmoney - Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        case '*':
+                            temp_money = lastmoney * Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        case '/':
+                            temp_money = lastmoney / Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
                         default:
                             break;
                     }
@@ -139,6 +279,131 @@ public class outlay extends AppCompatActivity {
             }
         }
     };
+
+    //-號鍵
+    Button.OnClickListener minus_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if(firstinput) {
+                inputmoney_tx.setText("輸入金額");
+            }
+            else{
+                if (contiOperate){
+                    int temp_money = 0;
+                    switch (lastOperate){
+                        case '+':
+                            temp_money = lastmoney + Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        case '-':
+                            temp_money = lastmoney - Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        case '*':
+                            temp_money = lastmoney * Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        case '/':
+                            temp_money = lastmoney / Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        default:
+                            break;
+                    }
+                    lastmoney = temp_money;
+                    inputmoney_tx.setText(String.valueOf(temp_money));
+                    contiOperate = false;
+                }
+                else {
+                    contiOperate = true;
+                    firstinput = false;
+                    lastOperate = '-';
+                    lastmoney = Integer.parseInt(inputmoney_tx.getText().toString());
+                    inputmoney_tx.setText("0");
+                }
+            }
+        }
+    };
+
+    //*號鍵
+    Button.OnClickListener times_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if(firstinput) {
+                inputmoney_tx.setText("輸入金額");
+            }
+            else{
+                if (contiOperate){
+                    int temp_money = 0;
+                    switch (lastOperate){
+                        case '+':
+                            temp_money = lastmoney + Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        case '-':
+                            temp_money = lastmoney - Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        case '*':
+                            temp_money = lastmoney * Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        case '/':
+                            temp_money = lastmoney / Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        default:
+                            break;
+                    }
+                    lastmoney = temp_money;
+                    inputmoney_tx.setText(String.valueOf(temp_money));
+                    contiOperate = false;
+                }
+                else {
+                    contiOperate = true;
+                    firstinput = false;
+                    lastOperate = '*';
+                    lastmoney = Integer.parseInt(inputmoney_tx.getText().toString());
+                    inputmoney_tx.setText("0");
+                }
+            }
+        }
+    };
+
+    //除號鍵
+    Button.OnClickListener divied_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if(firstinput) {
+                inputmoney_tx.setText("輸入金額");
+            }
+            else{
+                if (contiOperate){
+                    int temp_money = 0;
+                    switch (lastOperate){
+                        case '+':
+                            temp_money = lastmoney + Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        case '-':
+                            temp_money = lastmoney - Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        case '*':
+                            temp_money = lastmoney * Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        case '/':
+                            temp_money = lastmoney / Integer.parseInt(inputmoney_tx.getText().toString());
+                            break;
+                        default:
+                            break;
+                    }
+                    lastmoney = temp_money;
+                    inputmoney_tx.setText(String.valueOf(temp_money));
+                    contiOperate = false;
+                }
+                else {
+                    contiOperate = true;
+                    firstinput = false;
+                    lastOperate = '/';
+                    lastmoney = Integer.parseInt(inputmoney_tx.getText().toString());
+                    inputmoney_tx.setText("0");
+                }
+            }
+        }
+    };
+
+
     //清除鍵
     Button.OnClickListener clear_click = new Button.OnClickListener(){
         @Override
@@ -153,5 +418,58 @@ public class outlay extends AppCompatActivity {
         }
     };
 
+    //倒退鍵
+    Button.OnClickListener back_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            if (firstinput)
+                inputmoney_tx.setText("輸入金額");
+            else
+                if (inputmoney_tx.getText().toString().length()==1){
+                    inputmoney_tx.setText("0");
+                }
+                else {
+                    inputmoney_tx.setText(inputmoney_tx.getText().toString().substring(0, inputmoney_tx.getText().toString().length() - 1));
+                }
+
+        }
+    };
+
+    //等於鍵
+    Button.OnClickListener equals_click = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            if (firstinput)
+                inputmoney_tx.setText("輸入金額");
+            else {
+                if (contiOperate){
+                    result = 0;
+                    int temp_money = Integer.parseInt(inputmoney_tx.getText().toString());
+                    switch (lastOperate){
+                        case '+':
+                            result = lastmoney + temp_money;
+                            break;
+                        case '-':
+                            result = lastmoney - temp_money;
+                            break;
+                        case '*':
+                            result = lastmoney * temp_money;
+                            break;
+                        case '/':
+                            result = lastmoney / temp_money;
+                            break;
+                        default:
+                            break;
+                    }
+                    inputmoney_tx.setText(String.valueOf(result));
+                    contiOperate = false;
+                }
+                else {
+                    result = Integer.parseInt(inputmoney_tx.getText().toString());
+                    inputmoney_tx.setText(String.valueOf(result));
+                }
+            }
+        }
+    };
 
 }
